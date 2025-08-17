91700fad6f10d1f03d957a7037215902cf5f4952
Aliq92 <aliq.reggae@gmail.com>
2025-08-17 12:16:45 +0800

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,8 @@
+diff --git a//dev/null b/contact.js
+index 0000000000000000000000000000000000000000..d2198f6902b4cd31f65b6dd9336c35ae3d68993e 100644
+--- a//dev/null
++++ b/contact.js
+@@ -0,0 +1,3 @@
++document.addEventListener('DOMContentLoaded', () => {
++  console.log('Contact form script loaded.');
++});
